7081e0ffec02ea39a70529bb508afbd010ffc3d4
esuau <evan.suau@etu.u-pec.fr>
2018-12-13 12:53:48 +0100

    Initial commit

---
+++ src/main/java/io/evan/bigdata/twittergraphapi/TwitterGraphApiApplication.java
@@ -0,0 +1,14 @@
+package io.evan.bigdata.twittergraphapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TwitterGraphApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TwitterGraphApiApplication.class, args);
+	}
+
+}
+
